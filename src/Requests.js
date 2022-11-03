ba706f21bf884c04a325c642ece451fdd1b383cc
Milen_Mladenov <milen_s_mladenov@yahoo.co.uk>
2022-11-03 12:59:20 +0200

    main page

---
+++ src/Requests.js
@@ -0,0 +1,11 @@
+const key = '386e0662232344e169df09a44f090359';
+
+const requests = {
+    requestTrending: `https://api.themoviedb.org/3/trending/movie/day?api_key=${key}&language=en-US&page=1`,
+    requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+    requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+    requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+    requestPopular: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=horror&page=1&include_adult=false`,
+}
+
+export default requests;
