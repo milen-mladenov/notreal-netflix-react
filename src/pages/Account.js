d75eea2f56d657659bea174e29c0078ceabd386c
Milen_Mladenov <milen_s_mladenov@yahoo.co.uk>
2022-11-04 15:16:18 +0200

    user liked movies functionality

--- src/pages/Account.js
+++ src/pages/Account.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import SavedShows from "../components/SavedShows";
 
 function Account() {
   return (
-    <div>Account</div>
-  )
+    <>
+      <div className="w-full text-white">
+        <img
+          className="w-full h-[400px] object-cover"
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/f669a8f4-de1e-49d7-bb56-c9bd1f4a9069/02227bca-d2e5-4ad3-bd88-28ad16163fd5/BG-en-20221031-popsignuptwoweeks-perspective_alpha_website_small.jpg"
+          alt="/"
+        />
+        <div className="bg-black/60 fixed top-0 left-0 w-full h-[550px]"></div>
+        <div className="absolute top-[20%] p-4 md:p-8">
+          <h1 className="textx-3xl md:text-5xl font-bold">My Shows</h1>
+        </div>
+      </div>
+      <SavedShows />
+    </>
+  );
 }
 
-export default Account
+export default Account;
